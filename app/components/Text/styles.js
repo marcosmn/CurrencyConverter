5950d9e1d3ea8385839a39782c540af5ccd88f8c
marcosmn <marcosmnobrega@gmail.com>
2018-04-23 17:01:56 -0300

    iniciando o projeto do currency converter

--- app/components/Text/styles.js
+++
@@ -1,9 +0,0 @@
-import EStyleSheet from 'react-native-extended-stylesheet';
-
-export default EStyleSheet.create({
-  smartText: {
-    color: '$white',
-    textAlign: 'center',
-    fontSize: 12,
-  },
-});
