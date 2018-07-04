c74cb35406f9462de97c91f76c407070af7d3e21
Marcos Martins <30351698+marcosmn@users.noreply.github.com>
2018-07-04 11:39:31 -0300

    Create ClearButton.js

---
+++ app/components/Button/ClearButton.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Text, TouchableOpacity, Image, View } from 'react-native';
+
+import styles from './styles';
+
+const ClearButton = ({ text, onPress }) => (
+  <TouchableOpacity style={styles.container} onPress={onPress}>
+    <View style={styles.wrapper}>
+      <Image resizeMode="contain" style={styles.icon} source={require('./images/icon.png')} />
+      <Text style={styles.text}>{text}</Text>
+    </View>
+  </TouchableOpacity>
+);
+//// o appetize, snack e expo esta com bug para renderizar imagens
+/*ClearButton = ({ text, onPress }) => (
+  <TouchableOpacity style={styles.container} onPress={onPress}>
+    <View style={styles.wrapper}>
+      <Image resizeMode="contain" style={styles.icon} source={require('./images/icon.png')} />
+      <Text style={styles.text}>{text}</Text>
+    </View>
+  </TouchableOpacity>
+);*/
+
+ClearButton.propTypes = {
+  text: PropTypes.string,
+  onPress: PropTypes.func,
+};
+
+export default ClearButton;
