0f9097bc11a6f3f2f1de6280d17f33f157d0ad3a
Marcos Martins <30351698+marcosmn@users.noreply.github.com>
2018-07-04 11:38:19 -0300

    Create LastConverted.js

---
+++ app/components/Text/LastConverted.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Text } from 'react-native';
+import styles from './styles';
+const LastConverted = ({ date, base, quote, conversionRate }) => (
+  <Text style={styles.smallText}>
+    1 {base} = {conversionRate} {quote} as of {date.toDateString()}
+  </Text>
+);
+LastConverted.propTypes = {
+  date: PropTypes.object,
+  base: PropTypes.string,
+  quote: PropTypes.string,
+  conversionRate: PropTypes.number,
+};
+
+export default LastConverted;
