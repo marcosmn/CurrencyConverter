4e6318abcbe533b87271836ccbf9940405fbc677
Marcos Martins <30351698+marcosmn@users.noreply.github.com>
2018-07-04 11:50:08 -0300

    Create sagas.js

---
+++ app/config/sagas.js
@@ -0,0 +1,41 @@
+import { takeEvery, select, call, put } from 'redux-saga/effects';
+
+import {
+  SWAP_CURRENCY,
+  CHANGE_BASE_CURRENCY,
+  GET_INITIAL_CONVERSION,
+  CONVERSION_RESULT,
+  CONVERSION_ERROR,
+} from '../actions/currencies';
+
+const getLatestRate = currency => fetch(`https://frankfurter.app/current?from=${currency}`);
+
+function* fetchLatestConversionRates(action) {
+  try {
+    let { currency } = action;
+    if (currency === undefined) {
+      currency = yield select(state => state.currencies.baseCurrency);
+    }
+    console.log('currency', currency);
+
+    const response = yield call(getLatestRate, currency);
+    const result = yield response.json();
+
+    console.log('result', result);
+    if (result.error) {
+      yield put({ type: CONVERSION_ERROR, error: result.error });
+    } else {
+      yield put({ type: CONVERSION_RESULT, result });
+    }
+  } catch (err) {
+    yield put({ type: CONVERSION_ERROR, error: err.message });
+  }
+}
+
+function* rootSaga() {
+  yield takeEvery(GET_INITIAL_CONVERSION, fetchLatestConversionRates);
+  yield takeEvery(SWAP_CURRENCY, fetchLatestConversionRates);
+  yield takeEvery(CHANGE_BASE_CURRENCY, fetchLatestConversionRates);
+}
+
+export default rootSaga;
